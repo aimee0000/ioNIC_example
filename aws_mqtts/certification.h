01def569a3ab560b0a57641c05418fccca384e71
aimee <aimee@wiznet.io>
2026-02-12 09:25:21 +0900

    AWS MQTTS client pubsub example

---
+++ aws_mqtts/certification.h
@@ -0,0 +1,23 @@
+// rootca
+#define ROOTCA \                                
+    "-----BEGIN CERTIFICATE-----\r\n"\
+    "...\r\n"\
+    "...\r\n"\
+    "...\r\n"\
+    "-----END CERTIFICATE-----\r\n"
+
+// certificate crt
+#define CLEINT_CERT \                           
+    "-----BEGIN CERTIFICATE-----\r\n"\
+    "...\r\n"\
+    "...\r\n"\
+    "...\r\n"\
+    "-----END CERTIFICATE-----\r\n"
+
+// private key
+#define PRIVATE_KEY \                           
+    "-----BEGIN RSA PRIVATE KEY-----\r\n"\
+    "...\r\n"\
+    "...\r\n"\
+    "...\r\n"\
+    "-----END RSA PRIVATE KEY-----\r\n"
